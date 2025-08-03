2c5328c2107f713690c89ccb723545be6b001b31
rafael isac <rafael.darrigo@gmail.com>
2025-08-03 07:48:08 -0300

    creating_routes_and_controllers

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+export const home = (req: Request, res: Response) => {
+  res.send("Home no cotroller!");
+  //res.render('pages/page);
+};
+
+export const dogs = (req: Request, res: Response) => {
+  //res.render('pages/page);
+};
+export const cats = (req: Request, res: Response) => {
+  //res.render('pages/page);
+};
+export const fishes = (req: Request, res: Response) => {
+  //res.render('pages/page);
+};
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mustache from "mustache-express";
 import path from "path";
+import mainRoutes from "./routes/index";
 
 dotenv.config();
 
@@ -10,9 +11,15 @@ const server = express();
 server.set("view engine", "mustache");
 server.set("views", path.join(__dirname, "views"));
 server.engine("mustache", mustache());
+
 server.use(express.static(path.join(__dirname, "../public")));
 
-//Rotas
+server.use("/", mainRoutes);
+
+//Rota de página não encontrada
+server.use((req, res) => {
+  res.send("Página não encontrada");
+});
 
 server.listen(process.env.PORT, () => {
   console.log("Servidor rodando na porta http://localhost:3000");
