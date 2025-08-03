1df4c17e1bbb0234841b04a5a58337b62a9ec5b6
rafael isac <rafael.darrigo@gmail.com>
2025-08-03 09:16:48 -0300

    activating_menu

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
+import { createMenuObject } from "../helpers/createMenuObject";
 
 export const home = (req: Request, res: Response) => {
   res.render("pages/page", {
+    menu: createMenuObject("all"),
     banner: {
       title: "Todos os animais",
       background: "allanimals.jpg",
@@ -11,28 +13,28 @@ export const home = (req: Request, res: Response) => {
 
 export const dogs = (req: Request, res: Response) => {
   res.render("pages/page", {
+    menu: createMenuObject("dog"),
     banner: {
       title: "Cachorros",
       background: "banner_dog.jpg",
     },
   });
-  //res.render('pages/page);
 };
 export const cats = (req: Request, res: Response) => {
   res.render("pages/page", {
+    menu: createMenuObject("cat"),
     banner: {
       title: "Gatos",
       background: "banner_cat.jpg",
     },
   });
-  //res.render('pages/page);
 };
 export const fishes = (req: Request, res: Response) => {
   res.render("pages/page", {
+    menu: createMenuObject("fish"),
     banner: {
       title: "Peixes",
       background: "banner_fish.jpg",
     },
   });
-  //res.render('pages/page);
 };
