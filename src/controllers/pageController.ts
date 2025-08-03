4797d4eba6d403f6e340a07a715a5989b9c042db
rafael isac <rafael.darrigo@gmail.com>
2025-08-03 08:32:15 -0300

    inserting_layout

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,16 +1,18 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-  res.send("Home no cotroller!");
-  //res.render('pages/page);
+  res.render("pages/page");
 };
 
 export const dogs = (req: Request, res: Response) => {
+  res.send("pages/dogs");
   //res.render('pages/page);
 };
 export const cats = (req: Request, res: Response) => {
+  res.send("pages/cats");
   //res.render('pages/page);
 };
 export const fishes = (req: Request, res: Response) => {
+  res.send("pages/fishes");
   //res.render('pages/page);
 };
