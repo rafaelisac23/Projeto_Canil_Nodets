0826a6c29a466482c88dfe73f5b3a3d52b38b855
rafael isac <rafael.darrigo@gmail.com>
2025-08-03 12:20:27 -0300

    Make_search_and_404

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ server.use("/", mainRoutes);
 
 //Rota de página não encontrada
 server.use((req, res) => {
-  res.send("Página não encontrada");
+  res.render("pages/404");
 });
 
 server.listen(process.env.PORT, () => {
